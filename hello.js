004dbcdcfe89d1a3f7f0e01a9a377cadf3d8704b
christiantondo <christiantondo1@gmail.com>
2026-02-13 02:14:25 +0100

    hello user using ternary operator

--- hello.js
+++ hello.js
@@ -1,4 +1,14 @@
-console.log(process.argv);
+// console.log(process.argv);
 
-const greetings = "Node.js";
-console.log(`Hello ${greetings} world`)
+// const greetings = "Node.js";
+// console.log(`Hello ${greetings} world`);
+
+// const user = process.argv[2];
+// console.log(`Hello ${user}`);
+
+const arg = process.argv.splice(2);
+const user = arg[0];
+
+user
+    ? console.log(`Hello ${user}`)
+    : console.error("user not valid, please insert username");
